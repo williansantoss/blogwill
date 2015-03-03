f6e7480e31801415c8ca99fe25b5f55cf9aff231
williansantoss <willyan00@gmail.com>
2015-03-03 18:54:03 -0300

    correção menu, carousel funcionando, criação diretorio img e algumas alterações 2

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -17,18 +17,19 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		//Doctype
 		$view->Doctype('XHTML1_TRANSITIONAL');					
 		//Css
-	
-		$view->headLink()->prependStylesheet('css/bootstrap.css');
-		$view->headLink()->prependStylesheet('../css/bootstrap.css');
-	
-		$view->headLink()->prependStylesheet('../css/bootstrap-responsive.css');
-		$view->headLink()->prependStylesheet('css/bootstrap-responsive.css');			
-		//Js
-	   
-		$view->headScript()->prependFile('js/bootstrap.min.js');
-                
-	   $view->headScript()->prependFile('../js/jquery-2.1.3.min.js');
-		$view->headScript()->prependFile('../js/bootstrap.min.js');
+            $view->headLink()->prependStylesheet('css/bootstrap.css');
+            $view->headScript()->prependFile('js/bootstrap.min.js');
+            $view->headLink()->prependStylesheet('../css/bootstrap.css');
+            $view->headLink()->prependStylesheet('../css/bootstrap-responsive.css');
+            $view->headLink()->prependStylesheet('css/bootstrap-responsive.css');
+            $view->headScript()->prependFile('../js/bootstrap.min.js');
+            $view->headScript()->prependFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'); 
+            
+       if (strstr($_SERVER["HTTP_USER_AGENT"], "MSIE")) {        
+            $view->headScript()->prependFile('https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js');        
+        }else{
+   	       $view->headScript()->prependFile('js/jquery-2.1.3.min.js');  
+        }
 				
 	}
 
