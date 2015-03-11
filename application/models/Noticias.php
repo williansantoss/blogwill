aad62946e2bea818bfd2b5b70997386f04e16984
williansantoss <willyan00@gmail.com>
2015-03-10 22:53:24 -0300

    iniciando autenticacao de usuario

--- application/models/Noticias.php
+++ application/models/Noticias.php
@@ -1,17 +1,11 @@
 <?php
-class Application_Model_Noticias extends Zend_Db_Table_Abstract
-{
-
+class Application_Model_Noticias extends Zend_Db_Table_Abstract {
 	protected $_name = 'pg_inicial_noticias';
 	protected $_primary = 'id';
-	
-	public function listar(){
-	
-		try {		
-			return $this->fetchAll();
-			
-	} catch (Exception $e) {
-		
-	}
+	public function listar() {
+		try {
+			return $this->fetchAll ();
+		} catch ( Exception $e ) {
+		}
 	}
 }
